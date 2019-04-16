a9ba46ead33f1062f3953ac16389c3384aa086d3
IrenPischulina <iren.pischulina@gmail.com>
2019-04-16 14:16:04 +0700

    Исправлен код создания матрицы

--- game.cpp
+++ game.cpp
@@ -3,15 +3,15 @@
 Game::Game()
 {
     matrix = nullptr;
-
-}
-
-int **Game::getMatrix()
-{
     matrix = new int*[4];
     for ( int i = 0; i < 4; i ++)
     {
         matrix[i] = new int[4];
     }
+
+}
+
+int **Game::getMatrix()
+{
     return matrix;
 }
